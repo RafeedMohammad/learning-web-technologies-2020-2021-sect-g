f184ce5a807732a3015b1b6c26cdc676b0c2662f
Rafeed Mohammad <rafeedmohammad1107@gmail.com>
2021-02-16 00:01:14 +0600

    Getting Into PHP

---
+++ Mid Term/MID_LAB_TASK_PHP_04/task1.php
@@ -0,0 +1,17 @@
+<?php
+
+    $length = 20;
+    $width = 30;
+
+    $area = $length * $width;
+    $perimeter = 2 * ($length * $width);
+
+    
+    echo "Length Entered: ".$length;
+    echo "<br>";
+    echo "Width Entered: ".$width;
+    echo "<br>";
+    echo "Area: ".$area;
+    echo "<br>";
+    echo "Permiter: ".$perimeter;
+?>
