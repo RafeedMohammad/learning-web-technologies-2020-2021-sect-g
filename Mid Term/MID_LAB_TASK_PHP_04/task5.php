7a198b80bb0a19863de9935c5cfd4644675f85e1
Rafeed Mohammad <rafeedmohammad1107@gmail.com>
2021-02-16 03:28:56 +0600

    Done! Until Task6

---
+++ Mid Term/MID_LAB_TASK_PHP_04/task5.php
@@ -0,0 +1,19 @@
+<?php
+
+    //$num = 10;
+
+    for($num = 10; $num <=100; $num++)
+    {
+        if($num % 2 == 0)
+        {
+            continue;
+        }
+    
+        else
+        {
+            echo $num."<br>";
+        }
+    }
+
+
+?>
