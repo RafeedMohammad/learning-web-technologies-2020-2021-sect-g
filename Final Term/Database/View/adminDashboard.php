8fd2b2edbb8fecc4abe0452211ad93d2a6eee642
Rafeed Mohammad <rafeedmohammad1107@gmail.com>
2021-03-20 19:43:09 +0600

    Revisited dashboard with employee list

--- Final Term/Database/View/adminDashboard.php
+++ Final Term/Database/View/adminDashboard.php
@@ -1,13 +1,30 @@
 <?php
     session_start();
+
+        $connection = mysqli_connect('localhost', 'root', '');
+
+        mysqli_select_db($connection, 'medico');
+
+	
+
+        $sql = "SELECT * from user";
+        $result = mysqli_query($connection, $sql);
+        echo "<table border=1>
+                <tr>
+                    <td>ID</td>
+                    <td>Username</td>
+                    <td>Email</td>
+                    <td>Type</td>
+                </tr>";
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo 	"<tr>
+                        <td>{$row['ID']}</td>
+                        <td>{$row['Name']}</td>
+                        <td>{$row['Email']}</td>
+                        <td>{$row['Role']}</td>
+                    </tr>";
+        }
+        echo "</table>";
+
+
 ?>
-
-<html>
-    <head>
-        <title>Admin Dashboard</title>
-    </head>
-
-    <body>
-        Welcome to the Dashboard
-    </body>
-</html>
