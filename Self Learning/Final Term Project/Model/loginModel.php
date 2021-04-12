1eeb3c661090321d72714885bf26436b439e51db
RafeedMohammad <rafeedmohammad1107@gmail.com>
2021-04-12 13:41:49 -0700

    Employee Creation Done

--- Self Learning/Final Term Project/Model/loginModel.php
+++ Self Learning/Final Term Project/Model/loginModel.php
@@ -8,6 +8,7 @@
 		$result = mysqli_query($conn, $sql);
 
 		$row = mysqli_fetch_assoc($result);
+		$role = $row['Role'];
 
 		if(count($row) > 0)
 		{
@@ -16,6 +17,21 @@
 
 		else
 		{
+			return false;
+		}	
+
+	}
+
+	function insertEmployee($employee){
+		$conn = getConnection();
+		$sql = "INSERT INTO users VALUES('', '{$employee['fName']}', '{$employee['lName']}', '{$employee['email']}', '{$employee['phone']}', '{$employee['password']}', '{$employee['dob']}', '{$employee['role']}', '{$employee['gender']}', '{$employee['profile_picture']}', '{$employee['salary']}' )";
+
+		$result = mysqli_query($conn, $sql);
+	    
+
+		if($result){
+			return true;
+		}else{
 			return false;
 		}
 
